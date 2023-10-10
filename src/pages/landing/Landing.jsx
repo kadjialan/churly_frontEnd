4d380ffdc362168fec6d55e6009b6822689427fc
kadjialan <alankadji7@gmail.com>
2023-10-10 17:39:55 +0100

    dessigning landing page

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import './Landing.css'
+import React from "react";
+import "./Landing.css";
+import Hands from "../../assets/images/hands.jpg";
 
 export default function Landing() {
   return (
-    <div className='container'> 
-    <div id='bg'>
-      <p> hello world</p>
+    <div className="container">
+      <div id="bg">
+        <img src={Hands} alt="hands" className="picture" />
+        <div className="right">
+          <div className="right__content">
+            <p>We are here to help you live a faithful life with Christ</p>
+            <h1> PROVIDE TOP TESTIMONIALS AND INMPACT THE COMMUNITY</h1>
+            <div className="buttons_holder">
+              <button className="signup">SING UP</button>
+              <button className="signup">SING IN</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
